fa9e89f42cccc4a2d5daf989c1f28dfb2030bca9
Vivvie Lin <vvlnote@hotmail.com>
2019-07-27 18:07:38 -0700

    add the CatComponent, EinsteinQuoteComponent and MouseComponent into the App.js, and add "default" to expoort class EinsteinQuoteComponent

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+
 export default class CatComponent extends Component {
   render() {
     return (
       <div className="bar" id="cat">
         <img src="/cat.gif" />
+        
       </div>
     );
   }
 }
+
